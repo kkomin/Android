f424fc44c96eeb97a1b1d4e80c2888c1d077dc8d
kkomin <100822055+kkomin@users.noreply.github.com>
2023-08-27 17:04:08 +0900

    [Features] : Adapter 클래스 정의를 위한 함수 및 클래스 정의

--- ContactProgram/app/src/main/java/com/example/contactproject/MyAdapter.kt
+++ ContactProgram/app/src/main/java/com/example/contactproject/MyAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.contactproject
 
-class MyAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(val mItems : MutableList<MyItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    // 즐겨찾기 여부 viewType 상수 정의
+    private val TYPE_NORMAL = 0
+    private val TYPE_SPECIAL = 1
+
+    // 즐겨찾기 여부에 따른 viewHolder 생성
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    // 즐겨찾기 안했을 시 viewHolder 클래스 정의
+    class NormalHolder() {
+
+    }
+
+    // 즐겨찾기 시 viewHolder 클래스 정의
+    class SpecialHolder() {
+
+    }
 }
