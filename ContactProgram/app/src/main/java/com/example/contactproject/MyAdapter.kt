bf759d8dee256c39815a925a92e8af76d8c47a3d
kkomin <100822055+kkomin@users.noreply.github.com>
2023-08-27 17:12:07 +0900

    [Features] : Adapter 클래스 정의를 위한 함수 및 클래스 정의 수정 중... [UI] : 즐겨찾기 시의 레이아웃 생성 및 구현

--- ContactProgram/app/src/main/java/com/example/contactproject/MyAdapter.kt
+++ ContactProgram/app/src/main/java/com/example/contactproject/MyAdapter.kt
@@ -1,7 +1,9 @@
 package com.example.contactproject
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.contactproject.databinding.ItemRecycleBinding
 
 class MyAdapter(val mItems : MutableList<MyItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     // 즐겨찾기 여부 viewType 상수 정의
@@ -10,7 +12,12 @@ class MyAdapter(val mItems : MutableList<MyItem>) : RecyclerView.Adapter<Recycle
 
     // 즐겨찾기 여부에 따른 viewHolder 생성
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        return when(viewType) {
+            TYPE_NORMAL -> {
+                val binding = ItemRecycleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                NormalHolder(binding)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
@@ -22,8 +29,11 @@ class MyAdapter(val mItems : MutableList<MyItem>) : RecyclerView.Adapter<Recycle
     }
 
     // 즐겨찾기 안했을 시 viewHolder 클래스 정의
-    class NormalHolder() {
-
+    class NormalHolder(binding : ItemRecycleBinding) {
+        val image = binding.imageView
+        val name = binding.tvName
+        val number = binding.tvNumber
+        val favorite = binding.star
     }
 
     // 즐겨찾기 시 viewHolder 클래스 정의
