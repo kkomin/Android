3943821ad2bec3c494c500ac33f8826d12f648c8
kkomin <100822055+kkomin@users.noreply.github.com>
2023-08-27 15:35:15 +0900

    [UI] - activity_main에 recyclerView 추가 [AddData] - MyItem인 DataClass에 Data 추가

--- ContactProgram/app/src/main/java/com/example/contactproject/MyItem.kt
+++ ContactProgram/app/src/main/java/com/example/contactproject/MyItem.kt
@@ -1,4 +1,5 @@
 package com.example.contactproject
 
-class MyItem {
+// 사진, 이름, 전화번호, 즐겨찾기 여부 필드를 포함
+data class MyItem(val aIcon : Int, val aName : String, val aNumber : String, val aFavorite : Boolean){
 }
