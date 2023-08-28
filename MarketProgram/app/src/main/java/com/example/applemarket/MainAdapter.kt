b056a20991918760fd495551208383db87b2d1ce
kkomin <100822055+kkomin@users.noreply.github.com>
2023-08-28 16:59:52 +0900

    [FEATURE] : MainAdapter 클래스를 정의하기 위한 기본 틀 생성

--- MarketProgram/app/src/main/java/com/example/applemarket/MainAdapter.kt
+++ MarketProgram/app/src/main/java/com/example/applemarket/MainAdapter.kt
@@ -1,4 +1,22 @@
 package com.example.applemarket
 
-class MainAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.applemarket.databinding.ItemBinding
+
+class MainAdapter (val mItems : MutableList<ItemData>) : RecyclerView.Adapter<MainAdapter.Holder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        TODO("Not yet implemented")
+    }
+    inner class Holder(val binding : ItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
 }
