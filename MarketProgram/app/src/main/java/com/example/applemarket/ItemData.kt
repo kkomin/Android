2cb2d69b0707ba47111ec10348c8db3991c546d0
kkomin <100822055+kkomin@users.noreply.github.com>
2023-08-28 18:12:30 +0900

    [UI] : imageView - 모서리 둥근 정도 재설정 [FEATURE] : ItemData - 데이터클래스의 Data 설정

--- MarketProgram/app/src/main/java/com/example/applemarket/ItemData.kt
+++ MarketProgram/app/src/main/java/com/example/applemarket/ItemData.kt
@@ -1,3 +1,5 @@
 package com.example.applemarket
 
-data class ItemData() {}
+data class ItemData(val num : Int, val imageName : String, val name : String,
+                    val des : String, val seller : String, val price : Int,
+                    val address : String, val heart : Int, val chat : Int) {}
