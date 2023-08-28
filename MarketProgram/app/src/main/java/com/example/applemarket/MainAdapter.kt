d1fb55ff00d3e9179b2c78961b5c95edd5f91d67
kkomin <100822055+kkomin@users.noreply.github.com>
2023-08-28 17:51:57 +0900

    [UI] : imageView 모서리 둥글게 설정

--- MarketProgram/app/src/main/java/com/example/applemarket/MainAdapter.kt
+++ MarketProgram/app/src/main/java/com/example/applemarket/MainAdapter.kt
@@ -17,6 +17,9 @@ class MainAdapter (val mItems : MutableList<ItemData>) : RecyclerView.Adapter<Ma
         TODO("Not yet implemented")
     }
     inner class Holder(val binding : ItemBinding) : RecyclerView.ViewHolder(binding.root) {
-
+        val image = binding.imageView
+        val name = binding.tvName
+        val location = binding.tvLocation
+        val price = binding.tvPrice
     }
 }
