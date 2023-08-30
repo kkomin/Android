067616b711d032e4ab3d7c7aee82c71f78214c96
kkomin <100822055+kkomin@users.noreply.github.com>
2023-08-30 12:46:18 +0900

    [UI] : frament - 상세페이지 UI 구현 완료

--- MarketProgram/app/src/main/java/com/example/applemarket/DetailPage.kt
+++ MarketProgram/app/src/main/java/com/example/applemarket/DetailPage.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class DetailPage : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_page)
