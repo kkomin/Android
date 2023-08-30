13334f1208d6e685cd51de32cfa090e6e9414438
kkomin <100822055+kkomin@users.noreply.github.com>
2023-08-30 14:21:30 +0900

    [FEATURE] : DetailPage에 Fragment 연결

--- MarketProgram/app/src/main/java/com/example/applemarket/DetailPage.kt
+++ MarketProgram/app/src/main/java/com/example/applemarket/DetailPage.kt
@@ -2,11 +2,17 @@ package com.example.applemarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.applemarket.databinding.ActivityDetailPageBinding
 
 class DetailPage : AppCompatActivity() {
-
+    private val binding by lazy { ActivityDetailPageBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_page)
+        setContentView(binding.root)
+
+        // FirstFragment 추가
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_first, FirstFragment())
+        fragmentTransaction.commit()
     }
 }
