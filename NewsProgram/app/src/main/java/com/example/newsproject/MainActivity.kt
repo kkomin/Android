0f491d5a77978c9cf1bfbc3236a6c03cafaadcbe
kkomin <100822055+kkomin@users.noreply.github.com>
2023-08-31 18:15:02 +0900

    [UI] : RecyclerView ui 생성

--- NewsProgram/app/src/main/java/com/example/newsproject/MainActivity.kt
+++ NewsProgram/app/src/main/java/com/example/newsproject/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.newsproject
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentOrientation = resources.configuration.orientation
+        // 화면이 가로상태일 경우
+        if(currentOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+            TitleFragment()
+        }
+        // 화면이 세로일 경우
+        else {
+            DetailFragment()
+        }
     }
 }
