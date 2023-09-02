7604b321a2425c0f5007387f7037477673676997
kkomin <100822055+kkomin@users.noreply.github.com>
2023-09-02 19:08:41 +0900

    [FEATURE] : TitleFragment에서 항목 선택 시 DetailFragment로 이동

--- NewsProgram/app/src/main/java/com/example/newsproject/TitleFragment.kt
+++ NewsProgram/app/src/main/java/com/example/newsproject/TitleFragment.kt
@@ -6,31 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.newsproject.databinding.ActivityMainBinding
 import com.example.newsproject.databinding.FragmentTitleBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TitleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TitleFragment : Fragment() {
     private lateinit var binding: FragmentTitleBinding
-
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -41,13 +24,23 @@ class TitleFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentTitleBinding.inflate(inflater, container, false)
         val view = binding.root
-
         val newsData = NewsData()
         val adapter = MainAdapter(newsData)
 
         binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerView.adapter = adapter
 
+        adapter.itemClick = object : MainAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                // 항목 클릭시 처리
+                val selectedItem = newsData[position]
+                val detailFragment = DetailFragment.newInstance(selectedItem)
+                requireActivity().supportFragmentManager.beginTransaction()
+                    .replace(R.id.frameLayout, detailFragment)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
         return view
     }
 
@@ -172,21 +165,10 @@ class TitleFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FirstFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             TitleFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
