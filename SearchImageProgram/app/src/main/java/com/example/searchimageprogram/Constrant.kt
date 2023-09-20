40a2cdcbca6ebb6f6a9b589df966c58a59ab94c9
kkomin <100822055+kkomin@users.noreply.github.com>
2023-09-20 21:15:39 +0900

    [FEATURE] : Constrant로 url 및 apikey 값 설정 및 수정

--- SearchImageProgram/app/src/main/java/com/example/searchimageprogram/Constrant.kt
+++ SearchImageProgram/app/src/main/java/com/example/searchimageprogram/Constrant.kt
@@ -3,6 +3,6 @@ package com.example.searchimageprogram
 class Constrant {
     companion object {
         const val API_KEY = "4306e757a3c73ab1274bda8c58d629ff"
-        const val BASE_KEY = "https://dapi.kakao.com/v2/search/image"
+        const val BASE_URL = "https://dapi.kakao.com/v2/search/image/"
     }
 }
--- SearchImageProgram/app/src/main/java/com/example/searchimageprogram/retrofit/NetWorkInterface.kt
+++ SearchImageProgram/app/src/main/java/com/example/searchimageprogram/retrofit/NetWorkInterface.kt
@@ -3,18 +3,13 @@ package com.example.searchimageprogram.retrofit
 import com.example.searchimageprogram.Constrant
 import retrofit2.http.GET
 import retrofit2.http.Header
-import retrofit2.http.Query
 import retrofit2.http.QueryMap
 
 // 인터페이스 생성
 interface NetWorkInterface {
-    @GET(Constrant.BASE_KEY)
+    @GET(Constrant.BASE_URL)
     suspend fun searchImages (
-        @Header("Authorization") apiKey : String = Constrant.API_KEY,
+        @Header("Authorization") apiKey : String,
         @QueryMap param :HashMap<String, String>
-        /*@Query("query") query : String,
-        @Query("sort") sort : String,
-        @Query("page") page : Int,
-        @Query("size") size : Int*/
     ) : SearchResponse
 }
