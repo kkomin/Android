24fc649d560b92f12a5c602afe23a449cb5c6324
kkomin <100822055+kkomin@users.noreply.github.com>
2023-09-20 11:03:15 +0900

    [Feature] ⚙️ Interface 생성

--- SearchImageProgram/app/src/main/java/com/example/searchimageprogram/retrofit/NetWorkInterface.kt
+++ SearchImageProgram/app/src/main/java/com/example/searchimageprogram/retrofit/NetWorkInterface.kt
@@ -4,9 +4,10 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
 
+// 인터페이스 생성
 interface NetWorkInterface {
     @GET("https://dapi.kakao.com/v2/search/image")
-    suspend fun searchImage (
+    suspend fun searchImages (
         @Header("Authorization") apiKey : String = "4306e757a3c73ab1274bda8c58d629ff",
         @Query("query") query : String,
         @Query("sort") sort : String,
