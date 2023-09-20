3fbd2e9a9774758f9679c270295831de798cb88c
kkomin <100822055+kkomin@users.noreply.github.com>
2023-09-20 15:57:56 +0900

    [FEATURE] ⚙️ : bottomNavigationBar의 item 선택에 따른 fragment 변경 기능 추가

--- SearchImageProgram/app/src/main/java/com/example/searchimageprogram/MainActivity.kt
+++ SearchImageProgram/app/src/main/java/com/example/searchimageprogram/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.searchimageprogram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.searchimageprogram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        // 기본, main이 되는 fragment 설정
+        tranFragment(SearchFragment())
+
+        // 네비게이션 바 item 선택에 따른 fragment 설정
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.searchpage -> {
+                    tranFragment(SearchFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.savepage -> {
+                    tranFragment(SaveFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> false
+            }
+        }
+    }
+
+    // fragment 변경 함수 생성
+    private fun tranFragment(fragment : Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout, fragment)
+            .commit()
     }
 }
