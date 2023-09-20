77d5db650936ecf91c71da51205b38c4a2df1bbb
kkomin <100822055+kkomin@users.noreply.github.com>
2023-09-20 19:24:27 +0900

    [FIX] : API 키 값 Constrant 파일에 설정 및 적용

--- SearchImageProgram/app/src/main/java/com/example/searchimageprogram/retrofit/NetWorkInterface.kt
+++ SearchImageProgram/app/src/main/java/com/example/searchimageprogram/retrofit/NetWorkInterface.kt
@@ -1,5 +1,6 @@
 package com.example.searchimageprogram.retrofit
 
+import com.example.searchimageprogram.Constrant
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
@@ -7,13 +8,9 @@ import retrofit2.http.QueryMap
 
 // 인터페이스 생성
 interface NetWorkInterface {
-    companion object {
-        const val API_KEY = "4306e757a3c73ab1274bda8c58d629ff"
-    }
-
-    @GET("https://dapi.kakao.com/v2/search/image")
+    @GET(Constrant.BASE_KEY)
     suspend fun searchImages (
-        @Header("Authorization") apiKey : String = API_KEY,
+        @Header("Authorization") apiKey : String = Constrant.API_KEY,
         @QueryMap param :HashMap<String, String>
         /*@Query("query") query : String,
         @Query("sort") sort : String,
