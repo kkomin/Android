14f174baebaf1ee186aa3ec64714c3a0c52fbb86
kkomin <100822055+kkomin@users.noreply.github.com>
2023-09-22 09:58:38 +0900

    [FEATURE] : Fragment에 SaveAdapter 연결 [ERROR] : 로그에 빈 리스트가 찍히는 오류 발생

--- SearchImageProgram/app/src/main/java/com/example/searchimageprogram/Fragment/Save/SaveFragment.kt
+++ SearchImageProgram/app/src/main/java/com/example/searchimageprogram/Fragment/Save/SaveFragment.kt
@@ -1,34 +1,38 @@
 package com.example.searchimageprogram.Fragment.Save
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.searchimageprogram.R
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.searchimageprogram.Data.SearchData
+import com.example.searchimageprogram.MainActivity
 import com.example.searchimageprogram.databinding.FragmentSaveBinding
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class SaveFragment : Fragment() {
     private val binding by lazy { FragmentSaveBinding.inflate(layoutInflater) }
+    private var savedList : List<SearchData> = emptyList()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_save, container, false)
-    }
+        savedList = (activity as MainActivity).saveList
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SaveFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        binding.saveRecyclerview.adapter = SaveAdapter(requireContext()).apply {
+            mData = savedList.toMutableList()
+            Log.d("Data", "$mData")
+        }
+
+        binding.saveRecyclerview.apply {
+            adapter = SaveAdapter(requireContext())
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            setHasFixedSize(true)
+        }
+
+        // Inflate the layout for this fragment
+        return binding.root
     }
 }
